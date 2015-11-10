8fb2a85027b45cd5348a2828615fdf118ec0557d
Jonathan Zúñiga <jonathanzuniga@gmail.com>
2015-11-10 11:26:57 -0800

    Send a message like command line arguments in the messenger sender program.

--- MessageSender/Program.cs
+++ MessageSender/Program.cs
@@ -13,38 +13,42 @@ namespace MessageSender
 	class MessageSender
 	{
 		public const string API_KEY = Keys.GCM_SERVER_API_KEY;
-		public const string MESSAGE = "Hello, Xamarin!";
 
 		static void Main(string[] args)
 		{
-			var jGcmData = new JObject();
-			var jData = new JObject();
+			if (args.Length == 0) {
+				// Check for null array.
+				Console.WriteLine ("Please write the message to send.\nUsage is: MessageSender.exe [message]");
+			} else {
+				var jGcmData = new JObject ();
+				var jData = new JObject ();
 
-			jData.Add("message", MESSAGE);
-			jGcmData.Add("to", "/topics/global");
-			jGcmData.Add("data", jData);
+				jData.Add ("message", String.Join (Environment.NewLine, args));
+				jGcmData.Add ("to", "/topics/global");
+				jGcmData.Add ("data", jData);
 
-			var url = new Uri("https://gcm-http.googleapis.com/gcm/send");
+				var url = new Uri ("https://gcm-http.googleapis.com/gcm/send");
 
-			try {
-				using (var client = new HttpClient()) {
-					client.DefaultRequestHeaders.Accept.Add(
-						new MediaTypeWithQualityHeaderValue("application/json"));
+				try {
+					using (var client = new HttpClient ()) {
+						client.DefaultRequestHeaders.Accept.Add (
+							new MediaTypeWithQualityHeaderValue ("application/json"));
 
-					client.DefaultRequestHeaders.TryAddWithoutValidation(
-						"Authorization", "key=" + API_KEY);
+						client.DefaultRequestHeaders.TryAddWithoutValidation (
+							"Authorization", "key=" + API_KEY);
 
-					Task.WaitAll(client.PostAsync(url,
-						new StringContent(jGcmData.ToString(), Encoding.Default, "application/json"))
-						.ContinueWith(response => {
-							Console.WriteLine(response);
-							Console.WriteLine("Message sent: check the client device notification tray.");
+						Task.WaitAll (client.PostAsync (url,
+							new StringContent (jGcmData.ToString (), Encoding.Default, "application/json"))
+							.ContinueWith (response => {
+							Console.WriteLine (response);
+							Console.WriteLine ("Message sent: check the client device notification tray.");
 						})
-					);
+						);
+					}
+				} catch (Exception e) {
+					Console.WriteLine ("Unable to send GCM message:");
+					Console.Error.WriteLine (e.StackTrace);
 				}
-			} catch (Exception e) {
-				Console.WriteLine("Unable to send GCM message:");
-				Console.Error.WriteLine(e.StackTrace);
 			}
 		}
 	}
